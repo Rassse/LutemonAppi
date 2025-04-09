95f312b346eb3dc9fe09a84a0e2091c5df4bdf5c
Rasmus Kemppainen <rasmus.kemppainen@student.lut.fi>
2025-04-09 19:25:59 +0300

    Initial commit

---
+++ app/src/main/java/com/example/lutemonappi/Storage.java
@@ -0,0 +1,95 @@
+package com.example.lutemonappi;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import android.content.Context;
+
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public class Storage {
+    private String name;
+    private ArrayList<Lutemon> lutemons = new ArrayList<>();
+    private static Storage storage = null;
+    private Storage() {
+
+    }
+
+    public static Storage getInstance() {
+        if(storage == null) {
+            storage = new Storage();
+        }
+        return storage;
+    }
+
+    public ArrayList<Lutemon> getLutemons() {
+        return lutemons;
+    }
+
+    public void addLutemon(Lutemon lutemon) {
+        lutemons.add(lutemon);
+    }
+
+    public Lutemon getLutemonById(int id) {
+        return lutemons.remove(id);
+    }
+
+    public Lutemon getLutemonByIdWithoutRemove(int id) {
+        return lutemons.get(id);
+    }
+
+    public void removeLutemon(String id) {
+        int i = 0;
+        for (Lutemon l : lutemons) {
+            if (l.getId().equals(id)) {
+                break;
+            }
+            i++;
+        }
+        lutemons.remove(i);
+    }
+
+    public void listLutemons() {
+        int i = 0;
+        for (Lutemon lutemon : lutemons) {
+            System.out.println(i++ + ": " + lutemon.getName());
+        }
+    }
+
+    public void listLutemonsInformation() {
+        for (Lutemon lutemon : lutemons) {
+            lutemon.printSpecs();
+        }
+    }
+
+    public void saveLutemons(Context context) {
+        try {
+            ObjectOutputStrean lutemonWriter = new ObjectOutputStream(context.openFileOutput("lutemons.data", Context.MODE_PRIVATE));
+            lutemonWriter.writeObject(lutemons);
+            lutemonWriter.close();
+        } catch (IOException e) {
+            System.out.println("Lutemonien tallentaminen ei onnistunut!");
+        }
+
+    }
+
+    public void loadLutemons(Context context) {
+        try {
+            ObjectInputStream lutemonReader = new ObjectInputStream(context.openFileInput("lutemons.data"));
+            lutemons = (ArrayList<Lutemon>) lutemonReader.readObject();
+            lutemonReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Lutemonien lukeminen ei onnistunut!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            System.out.println("Lutemonien lukeminen ei onnistunut!");
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            System.out.println("Lutemonien lukeminen ei onnistunut!");
+            e.printStackTrace();
+        }
+    }
+}
+
