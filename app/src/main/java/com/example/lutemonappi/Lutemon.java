99f2fe6596dbd9c4f184894792ec1a5c0c615d9a
Rasmus Kemppainen <rasmus.kemppainen@student.lut.fi>
2025-04-09 20:25:25 +0300

    1.1

---
+++ app/src/main/java/com/example/lutemonappi/Lutemon.java
@@ -0,0 +1,80 @@
+package com.example.lutemonappi;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Lutemon implements Serializable {
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    protected String name;
+    protected String color;
+    protected int attack;
+    protected int defense;
+    protected int experience;
+    protected int health;
+    protected int maxHealth;
+    protected int id;
+}
--- app/src/main/java/com/example/lutemonappi/MainActivity.java
+++ app/src/main/java/com/example/lutemonappi/MainActivity.java
@@ -1,24 +1,36 @@
 package com.example.lutemonappi;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+    }
+
+    public void addLutemon(View view) {
+        EditText name = findViewById(R.id.editText);
+        RadioGroup rgLutemonType = findViewById(R.id.rgLutemonType);
+        switch (rgLutemonType.getCheckedRadioButtonId()) {
+            case R.id.radioButtonWhite:
+                Storage.getInstance().addLutemon(new Lutemon(name.getText().toString(), "Valkoinen"));
+            case R.id.radioButtonGreen:
+                Storage.getInstance().addLutemon(new Lutemon(name.getText().toString(), "Vihreä"));
+            case R.id.radioButtonPink:
+                Storage.getInstance().addLutemon(new Lutemon(name.getText().toString(), "Pinkki"));
+            case R.id.radioButtonOrange:
+                Storage.getInstance().addLutemon(new Lutemon(name.getText().toString(), "Oranssi"));
+            case R.id.radioButtonBlack:
+                Storage.getInstance().addLutemon(new Lutemon(name.getText().toString(), "Musta"));
+        }
     }
 }
