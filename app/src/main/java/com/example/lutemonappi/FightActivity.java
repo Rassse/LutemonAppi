de71aef71fa857290a9bfe67d725ca985e2264ee
Rasmus Kemppainen <rasmus.kemppainen@student.lut.fi>
2025-04-21 13:08:55 +0300

    2.0

---
+++ app/src/main/java/com/example/lutemonappi/FightActivity.java
@@ -0,0 +1,12 @@
+package com.example.lutemonappi;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class FightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fight);
+    }
+}
